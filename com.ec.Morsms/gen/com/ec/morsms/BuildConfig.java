44e0dc32102bab9bcd77488cf3fa65f2fd36cdd8
Quentin Li <quentinl>
2012-12-02 17:48:55 -0500

    Setup incoming SMS reading

---
+++ com.ec.Morsms/gen/com/ec/morsms/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ec.morsms;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ com.ec.Morsms/src/com/example/com/ec/morsms/Morsms.java
@@ -0,0 +1,20 @@
+package com.example.com.ec.morsms;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class Morsms extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	public void onResume(){
+		super.onResume();
+	}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+}
