d6085fa5498c2d1896f008f10f604414657d47d8
aresen <aresen@github.com>
2012-12-13 21:50:33 -0500

    Version 4 of package updated in AndroidManifest.xml

--- MorSMS/src/com/ec/morsms/Settings.java
+++ MorSMS/src/com/ec/morsms/Settings.java
@@ -2,7 +2,6 @@ package com.ec.morsms;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
